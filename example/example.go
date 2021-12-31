4e9ddeeed723045a8295ca4bf5d5fd1e80df8a6c
neelanjan00 <neelanjan@chaosnative.com>
2021-12-31 17:46:27 +0530

    added examples
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- example/example.go
+++ example/example.go
@@ -1,11 +1,47 @@
 package main
 
 import (
+	"fmt"
+
 	gapBuffer "github.com/neelanjan00/gap-buffer"
 )
 
 func main() {
 
+	// initialise a GapBuffer struct
 	gb := new(gapBuffer.GapBuffer)
 
+	// initialise a string to the buffer
+	gb.SetString("A Buffer")
+
+	// display the text from buffer
+	fmt.Println(gb.GetString())
+
+	// move the cursor to right
+	gb.MoveCursorRight()
+
+	// insert character
+	gb.Insert(' ')
+	gb.Insert('G')
+	gb.Insert('a')
+	gb.Insert('p')
+
+	fmt.Println(gb.GetString())
+
+	// move cursor to left
+	gb.MoveCursorLeft()
+	gb.MoveCursorLeft()
+	gb.MoveCursorLeft()
+
+	// backspace
+	gb.Backspace()
+
+	fmt.Println(gb.GetString())
+
+	gb.MoveCursorLeft()
+
+	// delete
+	gb.Delete()
+
+	fmt.Println(gb.GetString())
 }
